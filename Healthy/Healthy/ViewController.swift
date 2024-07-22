5ab1c4446bd2687731953a8bb3dcefdbb0e97696
Alex Neumark <alexneumark@Alexs-Air.IGD_Rostelecom>
2024-07-22 18:32:30 +0300

    Montserrat font added

--- Healthy/Healthy/ViewController.swift
+++ Healthy/Healthy/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var testLabel: UILabel = {
+        let testLabel = UILabel()
+        testLabel.text = "Hello, world!"
+        testLabel.textColor = .white
+        testLabel.font = .systemFont(ofSize: 36)
+        testLabel.textAlignment = .center
+        
+        return testLabel
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
+        configureTest()
+        
+    }
+    
+    func configureTest() {
+        view.addSubview(testLabel)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            testLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            testLabel.widthAnchor.constraint(equalToConstant: 200.0),
+            testLabel.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
     }
 
 
