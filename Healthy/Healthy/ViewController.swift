b9f0afcfedc08d0246e5879c73f6510ee8c1882f
Alex Neumark <alexneumark@Alexs-Air.IGD_Rostelecom>
2024-07-22 17:41:59 +0300

    Project created

---
+++ Healthy/Healthy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Healthy
+//
+//  Created by Alex Neumark on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
